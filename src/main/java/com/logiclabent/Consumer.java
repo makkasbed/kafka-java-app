2f860ec6ba4b9d484c678996f9530891001f947a
adu <aluta182004@gmail.com>
2023-03-21 17:22:01 +0000

    added consumer

--- src/main/java/com/logiclabent/Consumer.java
+++ src/main/java/com/logiclabent/Consumer.java
@@ -1,4 +1,12 @@
 package com.logiclabent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Consumer {
+    private static final Logger log= LoggerFactory.getLogger(Consumer.class);
+
+    public static void main(String [] args)
+    {
+
+    }
 }
