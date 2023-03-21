0145f7fb2870abe87f82de92b4c5f920e40b575f
adu <aluta182004@gmail.com>
2023-03-21 16:53:28 +0000

    started work on Kafka java app

---
+++ src/main/java/com/logiclabent/HelloKafka.java
@@ -0,0 +1,13 @@
+package com.logiclabent;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+public class HelloKafka {
+    private static final Logger log = LoggerFactory.getLogger(HelloKafka.class);
+
+    public static void main(String [] args)
+    {
+        log.info("Hello Kafka");
+    }
+}
