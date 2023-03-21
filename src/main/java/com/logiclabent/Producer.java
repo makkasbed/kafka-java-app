40cf7069604900b788e984cf60da0019afa969a8
adu <aluta182004@gmail.com>
2023-03-21 16:55:26 +0000

    added logs to producer

--- src/main/java/com/logiclabent/Producer.java
+++ src/main/java/com/logiclabent/Producer.java
@@ -1,4 +1,13 @@
 package com.logiclabent;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Producer {
+    private static final Logger log= LoggerFactory.getLogger(Producer.class);
+
+    public static void main(String [] args)
+    {
+        log.info("This class will produce messages to Kafka");
+    }
 }
