6298ea2ee9931a31c428bcbf182b753c11c0ca53
adu <aluta182004@gmail.com>
2023-03-21 17:26:40 +0000

    implemented the consumer config

--- src/main/java/com/logiclabent/Consumer.java
+++ src/main/java/com/logiclabent/Consumer.java
@@ -2,11 +2,19 @@ package com.logiclabent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Properties;
+
 public class Consumer {
     private static final Logger log= LoggerFactory.getLogger(Consumer.class);
 
     public static void main(String [] args)
     {
        log.info("Consumer started");
+        Properties properties=new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG,"lotr_consumer_group");
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"earliest");
     }
 }
